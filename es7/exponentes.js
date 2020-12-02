d7847f116db3f619763f6eff73f56878aa342b55
pepe <lalo_dimo@hotmail.com>
2020-12-02 13:31:21 -0600

    agregando ahora los cambios en es7

---
+++ es7/exponentes.js
@@ -0,0 +1,6 @@
+let base=2;
+let exponente=4;
+
+let result=base**exponente;
+
+console.log(result);
---
+++ es7/includes.js
@@ -0,0 +1,11 @@
+let numeros=[23,2,3,4];
+
+if(numeros.includes(3))
+{
+    console.log("si lo contiene");
+}
+
+else
+{
+    console.log("no lo contiene");
+}
